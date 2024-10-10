c56e51de118dfdc4013128f648114115f63dcacc
ViniciusCostaGandolfi <vinicostagandolfi@gmail.com>
2024-10-10 13:05:20 -0300

    Altera https para http

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     'GOOGLE_MAPS_API_KEY':'AIzaSyA2J8XefDFWdILAPuQUwzqOAhLoMjYeQSw',
-    'ROTAFOOD_API':'https://rotafood-api-production.up.railway.app',
+    'ROTAFOOD_API':'http://rotafood-api-production.up.railway.app',
 };
